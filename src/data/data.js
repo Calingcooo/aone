c0f571407bf7b928bf93b3f36cb47b24e54120ea
Jhon Calingco <calingco.je@gmail.com>
2022-09-16 00:49:08 +0800

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+import image1 from '../assets/image1.webp'
+import image2 from '../assets/image2.webp'
+import image3 from '../assets/image3.webp'
+import image4 from '../assets/image4.webp'
+
+
+
+
+const data = [
+  {
+    id: 1,
+    image: image1,
+  },
+  {
+    id: 2,
+    image: image2,
+  },
+  {
+    id: 3,
+    image: image3,
+  },
+  {
+    id: 4,
+    image: image4,
+  },
+];
+
+export default data;
