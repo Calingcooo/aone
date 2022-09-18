d1f1f0e2ff18fe70ca7107da5a4fd7e68216af28
Jhon Calingco <calingco.je@gmail.com>
2022-09-18 20:00:46 +0800

    added components

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+//Assets
+import videoBG from '../assets/videos/aoneVideoBG.mp4'
+//Components
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+
+
+function Home() {
+
+  return (
+    <> 
+      <Navbar />
+        <div className='mx-2 my-2 drop-shadow-lg'>      
+          <video src={videoBG} autoPlay loop muted className='w-full h-full object-cover rounded-lg'></video>
+        </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
