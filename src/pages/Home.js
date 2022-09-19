10047a395b74b8979c69841f09184b0ad2150b24
Jhon Calingco <calingco.je@gmail.com>
2022-09-19 16:23:47 +0800

    Added slider component for studio one

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import videoBG from '../assets/videos/aoneVideoBG.mp4'
 
 //Components
 import Navbar from '../components/Navbar'
+import StudioOneGallery from '../components/StudioOneGallery'
 import Footer from '../components/Footer'
 
 
@@ -11,10 +12,13 @@ import Footer from '../components/Footer'
 function Home() {
 
   return (
-    <> 
+    <>
       <Navbar />
         <div className='mx-2 my-2 drop-shadow-lg'>      
           <video src={videoBG} autoPlay loop muted className='w-full h-full object-cover rounded-lg'></video>
+        </div>
+        <div>
+        <StudioOneGallery />
         </div>
       <Footer />
     </>
